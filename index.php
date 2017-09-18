81ccbf7860b5ad8e9f481a2e453ab302ed8a3682
BastiRenken <sebastian-renken@t-online.de>
2017-09-18 21:57:43 +0200

    Update

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <html lang="de">
 <head>
   <meta charset="utf-8" name="viewport" content="width=device-width"/>
-  <title> Smart Home </title>
+  <title>Smart Home</title>
   <link href="style.css" rel="stylesheet">
   <?php
   $gpios  = array(2, 3, 4);
+  $andere_gpios = array(17, 27, 22, 10, 9);
   foreach($gpios as $pin){
     shell_exec(sprintf("/usr/local/bin/gpio -g mode %d out", $pin));
+  }
+  foreach($andere_gpios as $pin){
+    shell_exec(sprintf("/usr/local/bin/gpio -g mode %d out", $pin));
+    shell_exec(sprintf("/usr/local/bin/gpio -g write %d 1", $pin));
   }
   shell_exec("/usr/local/bin/gpio -g mode 11 out");
 
@@ -59,6 +64,23 @@
     shell_exec("/usr/local/bin/gpio -g write 4 0");
     file_put_contents("farbe.txt", "hotpink");
   }
+  else if(isset($_GET["wecker"])){
+    file_put_contents("wecker.txt", $_GET["zeit"]);
+  }
+  /*
+  else if(isset($_GET["shuffle"])){
+    while($_GET = "shuffle"){
+      $pin = rand(2, 4);
+      $modus = rand(0, 1);
+      shell_exec(sprintf("/usr/local/bin/gpio -g write %d %d", $pin, $modus));
+      sleep(1);
+    }
+  }
+  else if(isset($_GET["wecker"])){
+    shell_exec("python3 /var/www/html/wecker.py");
+    echo("Wecker an");
+  }
+  */
   else if(isset($_GET["buzzer"])){
     shell_exec("/usr/local/bin/gpio -g write 11 1");
     sleep(1);
@@ -68,7 +90,7 @@
 </head>
 <?php $farbe = file_get_contents("farbe.txt"); ?>
 <?php echo"<body style=\"background-color:$farbe\">";?>
-  <h1>LED Control</h1>
+  <h1><a href="index.php">LED Control</a></h1>
   <div id=menu>
   <form method="get" action="index.php">
     <input class="button" type="submit" value="Aus" name="off">
@@ -87,7 +109,19 @@
     <p/>
     <input class="button" type="submit" value="Pink" name="pink">
     <p/>
+    <!-- <input class="button" type="submit" value="Shuffle" name="shuffle">
+    <p/> -->
+    <!--<input class="button" type="submit" value="Wecker" name="wecker">
+    <p/> -->
     <input class="button" type="submit" value="Buzzer" name="buzzer">
+    <p/>
+  </form>
+  <form action="index.php">
+    <p>
+      Weckzeit:
+      <input type="text" name="zeit">
+      <input type="submit" name="wecker"> 
+    </p>
   </form>
   </div>
 </body>

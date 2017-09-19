3403004819f6c1e9a3db6e23445e5cac5239cf7f
BastiRenken <sebastian-renken@t-online.de>
2017-09-19 21:33:04 +0200

    Update

--- wecker.py
+++ wecker.py
@@ -12,39 +12,47 @@ GPIO.setup(3, GPIO.OUT) # Grün
 zeit = time.strftime("%H:%M");
 status = open("wecker.txt", "r")
 status = status.read()
-a = 1
 
 if status != "aus":
-	while a == 1:
-		if zeit == status:
-			for i in range(5):
-				for i in range(2):
-					GPIO.output(3, GPIO.LOW)
-					GPIO.output(5, GPIO.LOW)
-					GPIO.output(7, GPIO.LOW)
-					time.sleep(0.1)
-					GPIO.output(3, GPIO.HIGH)
-					GPIO.output(5, GPIO.HIGH)
-					GPIO.output(7, GPIO.HIGH)
-					time.sleep(0.1)
-				GPIO.output(3, GPIO.HIGH)
-				for j in range(4):
-					GPIO.output(5, GPIO.LOW)
-					GPIO.output(23, GPIO.HIGH)
-					GPIO.output(7, GPIO.HIGH)
-					time.sleep(0.4)
-					GPIO.output(5, GPIO.HIGH)
-					GPIO.output(23, GPIO.LOW)
-					GPIO.output(7, GPIO.LOW)
-					time.sleep(0.4)
-				GPIO.output(3, GPIO.LOW)
-				time.sleep(0.5)
-				GPIO.output(5, GPIO.LOW)
-				GPIO.output(3, GPIO.HIGH)
-				time.sleep(0.5)
+	if zeit == status:
+		for i in range(2):
 			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.5)
 			GPIO.output(5, GPIO.LOW)
+			time.sleep(0.5)
+			GPIO.output(3, GPIO.HIGH)
 			GPIO.output(7, GPIO.LOW)
-			a = 0
-		zeit=time.strftime("%H:%M")
-		time.sleep(10)
+			time.sleep(0.5)
+			GPIO.output(5, GPIO.HIGH)
+			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.5)
+			GPIO.output(3, GPIO.HIGH)
+			GPIO.output(7, GPIO.HIGH)
+		for i in range(5):
+			for i in range(2):
+				GPIO.output(3, GPIO.LOW)
+				GPIO.output(5, GPIO.LOW)
+				GPIO.output(7, GPIO.LOW)
+				time.sleep(0.1)
+				GPIO.output(3, GPIO.HIGH)
+				GPIO.output(5, GPIO.HIGH)
+				GPIO.output(7, GPIO.HIGH)
+				time.sleep(0.1)
+			GPIO.output(3, GPIO.HIGH)
+			for j in range(3):
+				GPIO.output(5, GPIO.LOW)
+				GPIO.output(23, GPIO.HIGH)
+				GPIO.output(7, GPIO.HIGH)
+				time.sleep(0.4)
+				GPIO.output(5, GPIO.HIGH)
+				GPIO.output(23, GPIO.LOW)
+				GPIO.output(7, GPIO.LOW)
+				time.sleep(0.4)
+			GPIO.output(3, GPIO.LOW)
+			time.sleep(0.5)
+			GPIO.output(5, GPIO.LOW)
+			GPIO.output(3, GPIO.HIGH)
+			time.sleep(0.5)
+		GPIO.output(3, GPIO.LOW)
+		GPIO.output(5, GPIO.LOW)
+		GPIO.output(7, GPIO.LOW)

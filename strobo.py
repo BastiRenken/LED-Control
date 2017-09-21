ef3df479787aefa72db90a1e224ea6afe8374e04
BastiRenken <sebastian-renken@t-online.de>
2017-09-21 16:19:31 +0200

    New file

---
+++ strobo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(23, GPIO.OUT) # Buzzer
+GPIO.setup(7, GPIO.OUT) # Blau
+GPIO.setup(5, GPIO.OUT) # Rot
+GPIO.setup(3, GPIO.OUT) # Grün
+
+for a in range(5):
+    for b in range(2): # Rot-Grün-Blau
+        GPIO.output(3, GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(3, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(7, GPIO.LOW)
+        GPIO.output(5, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(7, GPIO.HIGH)
+    for c in range(2): # Strobo Weiss
+        GPIO.output(3, GPIO.LOW)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(7, GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(3, GPIO.HIGH)
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(7, GPIO.HIGH)
+        time.sleep(0.1)
+
+time.sleep(3)
+for d in range(4):
+    time.sleep(3)
+    for e in range(2):
+        GPIO.output(3, GPIO.LOW)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(7, GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(3, GPIO.HIGH)
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(7, GPIO.HIGH)
+        time.sleep(0.1)
